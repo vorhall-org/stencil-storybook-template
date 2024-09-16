34e394a10c34379c525ef9407d352cafd65b05c1
Yves Tscherry <info@feerglas.com>
2024-09-16 17:50:08 +0200

    chore: uninstall chromatic and test addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,16 +3,12 @@ const config = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
   ],
   framework: {
     name: '@storybook/web-components-vite',
     options: {},
   },
-  stories: [
-    '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 };
 
 export default config;
