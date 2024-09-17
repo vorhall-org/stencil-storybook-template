0291b5b0db9537b5e9ee1b2504d9827fd7facc63
Yves Tscherry <info@feerglas.com>
2024-09-17 15:07:38 +0200

    test: set config to test multiple browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,7 @@
-import { expect } from '@playwright/test';
+import {
+  devices,
+  expect,
+} from '@playwright/test';
 import {
   createConfig,
   matchers,
@@ -7,6 +10,21 @@ import {
 expect.extend(matchers);
 
 export default createConfig({
+  fullyParallel: true,
+  projects: [
+    {
+      name: 'chromium',
+      use: devices['Desktop Chrome'],
+    },
+    {
+      name: 'firefox',
+      use: devices['Desktop Firefox'],
+    },
+    {
+      name: 'Mobile Chrome',
+      use: devices['Pixel 5'],
+    },
+  ],
   testDir: './src/',
 });
 
