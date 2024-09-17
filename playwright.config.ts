a5ab76b5edeec21b7aa4a478d9810cd65288934d
Yves Tscherry <info@feerglas.com>
2024-09-17 16:28:39 +0200

    test: set config to only test one config

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,49 +9,56 @@ import {
 
 expect.extend(matchers);
 
+const projects = [
+  {
+    name: 'chromium',
+    use: devices['Desktop Chrome'],
+  },
+  {
+    name: 'Microsoft Edge',
+    use: {
+      ...devices['Desktop Edge'],
+      channel: 'msedge',
+    },
+  },
+  {
+    name: 'chromium',
+    use: {
+      ...devices['Desktop Chrome'],
+      viewport: {
+        height: 480,
+        width: 800,
+      },
+    },
+  },
+  {
+    name: 'firefox',
+    use: devices['Desktop Firefox'],
+  },
+  {
+    name: 'firefox',
+    use: {
+      ...devices['Desktop Firefox'],
+      viewport: {
+        height: 480,
+        width: 800,
+      },
+    },
+  },
+  {
+    name: 'Mobile Chrome',
+    use: devices['Pixel 5'],
+  },
+];
+
 export default createConfig({
   fullyParallel: true,
-  projects: [
-    {
-      name: 'chromium',
-      use: devices['Desktop Chrome'],
-    },
-    {
-      name: 'Microsoft Edge',
-      use: {
-        ...devices['Desktop Edge'],
-        channel: 'msedge',
-      },
-    },
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        viewport: {
-          height: 480,
-          width: 800,
-        },
-      },
-    },
-    {
-      name: 'firefox',
-      use: devices['Desktop Firefox'],
-    },
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-        viewport: {
-          height: 480,
-          width: 800,
-        },
-      },
-    },
-    {
-      name: 'Mobile Chrome',
-      use: devices['Pixel 5'],
-    },
-  ],
+
+  /**
+   * during development of tests, it makes senese to not run test in every
+   * browser config.
+   */
+  projects: [projects[0]],
   testDir: './src/',
 });
 
