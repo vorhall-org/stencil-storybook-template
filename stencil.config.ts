b3eb56a5d739a0b88ae0c82e5531487829459a54
Yves Tscherry <info@feerglas.com>
2024-09-19 10:26:20 +0200

    build: add and configure react-output-target

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,5 +1,6 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
+import { reactOutputTarget } from '@stencil/react-output-target';
 import projectConfig from './project-config';
 import eventSync from './stencil-build-helpers/rollup/event-sync';
 
@@ -15,6 +16,28 @@ export const config: Config = {
       externalRuntime: false,
       type: 'dist-custom-elements',
     },
+    reactOutputTarget({
+      // Relative path to where the React components will be generated
+      outDir: 'dist/react',
+
+      /**
+       * SUPER HACKY... /)
+       * stencil decided to step away from extensinve configs
+       * for the components and defineCustomElements import
+       * for the generated wrapper.
+       *
+       * Theyr approach is working if you work in a monorepo,
+       * so you can save the wrapper in the Dir of the consuming
+       * app.
+       *
+       * If you want an independent component library (saving the
+       * wrapper to the dist folder) not sitting
+       * next to the consumer app, this approach does not work
+       * since you'll have the stencil library name as a starting
+       * segment in the import of the components.
+       */
+      stencilPackageName: '../..',
+    }),
     {
       type: 'docs-readme',
     },
